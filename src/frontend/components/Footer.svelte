67b980e196514c6a5286e70c33c7b614080c43d4
Grayson Gullion <xinflux@gmail.com>
2021-11-03 20:34:52 -0400

    get ready for tailwind

--- src/frontend/components/Footer.svelte
+++ src/frontend/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer>
-  
+  <button class="">Launch</button>
 </footer>
 
 <style>
@@ -12,5 +12,10 @@
     place-items: center flex-end;
     background-color: darkslategrey;
     padding: 0 2em;
+  }
+
+  button {
+    background-color: var(--color-green);
+    text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
   }
 </style>
