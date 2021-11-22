0cc674aa7ed8511abd319df89c2b165934e38132
Grayson Gullion <xinflux@gmail.com>
2021-11-22 16:33:45 -0500

    clean up header search bar

--- src/frontend/components/TextInput.svelte
+++ src/frontend/components/TextInput.svelte
@@ -4,6 +4,7 @@
   export let placeholder = undefined
   export let value
   export let small = false
+  export let main = false
 
   const dispatch = createEventDispatcher()
 
@@ -13,7 +14,7 @@
   
 </script>
 
-<input class:small {placeholder} type="text" bind:value on:change={handleOnChange}/>
+<input class:small class:main {placeholder} type="text" bind:value on:change={handleOnChange}/>
 
 <style>
   input {
@@ -24,15 +25,27 @@
     width: 100%;
     color: var(--grey-100);
     font-size: 1.2em;
+    font-weight: 100;
     outline: none;
+  }
+
+  input::placeholder {
+    color: var(--grey-100);
+    opacity: 0.33;
   }
 
   input.small {
     font-size: 1em;
   }
 
+  input.main {
+    border: 1px solid transparent;
+    background-color: var(--grey-500);
+  }
+
+
   input:focus {
     background-color: var(--grey-500);
-    border-color: var(--primary-700);
+    border-color: var(--primary-500);
   }
 </style>
