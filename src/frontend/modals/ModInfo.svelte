8380fcde1af6e322b114443e288428f93fabc69b
Grayson Gullion <xinflux@gmail.com>
2021-12-04 19:47:28 -0500

    clean up modal styles

--- src/frontend/modals/ModInfo.svelte
+++ src/frontend/modals/ModInfo.svelte
@@ -3,6 +3,7 @@
   import { fade, fly } from "svelte/transition";
   import { state, closeDetails } from "../state/store";
   import { clickOutside } from "../helpers/clickoutside";
+  import Button from "../components/Button.svelte";
 
   let show = false
   let mod;
@@ -21,6 +22,12 @@
     closeDetails()
   }
 
+  const tagClicked = (e) => {
+    const text = e.detail.target.innerText
+    $state.filter = text
+    closeDetails()
+  }
+
 </script>
 
 {#if show}
@@ -35,47 +42,70 @@
       in:fly={{ y: -200, duration: 200 }}
       out:fly={{ y: 200, duration: 200 }}>
 
-      <h3>{mod.name} <span>({mod.version})</span></h3>
-      <p>Mod Id: <b>{mod.id}</b></p>
-      <p>Author(s): <b>{mod.author}</b></p>
-      <p>File Name: <b>{mod.fileName}</b></p>
-      <p>Install Dir: <b>{mod.installDir}</b></p>
-      <p>Description: <b>{mod.description}</b></p>
-      <p>Tags: <b>{mod.tags?.join(", ")}</b></p>
+      <h2>{mod.name} <span>({mod.version})</span></h2>
+      <p><span>Mod Id:</span> <b>{mod.id}</b></p>
+      <p><span>Author(s):</span> <b>{mod.author}</b></p>
+      <p><span>File Name:</span> <b>{mod.fileName}</b></p>
+      <p><span>Install Dir:</span> <b>{mod.installDir}</b></p>
+      <p><span>Description:</span> <b>{mod.description}</b></p>
+      <div>
+        <span>Tags:</span>
+        
+        {#each mod.tags as tag}
+          <Button small on:click={tagClicked}>{tag}</Button>
+        {/each}
+        
+      </div>
     </div>
   </div>
 {/if}
 
 <style>
-  h3 {
+  h2 {
     margin-bottom: 1em;
     display: flex;
-    align-items: center;
-    gap: 0.5em;
+    align-items: baseline;
+    gap: 0.33em;
+    line-height: 1;
   }
 
-  p {
+  p,.modal div {
     display: flex;
     gap: 1em;
-    margin-bottom: 0.25em;
-    padding: 0.25em 0;
+    padding: 0.5em 0;
     border-bottom: 2px solid var(--grey-900);
     line-height: 1.5;
+    align-items: baseline;
+  }
+
+  .modal div {
+    align-items: center;
+    gap: 0.5em;
+    flex-wrap: wrap;
+  }
+
+  .modal div :global(button) {
+    cursor: pointer;
   }
 
   b {
     font-size: 0.8em;
   }
 
-  h3 span {
-    font-size: 0.7em;
+  p span, p b  {
+    display: inline-block;
+  }
+
+
+  h2 span {
+    font-size: 0.5em;
     opacity: 0.5;
   }
   
   .bg {
     position: fixed;
     display: grid;
-    z-index: 10000;
+    z-index: 10010;
     inset: 0;
     background-color: rgba(0,0,0,0.5);
     backdrop-filter: blur(3px);
@@ -91,5 +121,7 @@
     color: var(--grey-100);
     background-color: var(--grey-500);
     box-shadow: 0 0 2em rgba(0,0,0,0.8);
+    max-height: 80vh;
+    overflow-y: auto;
   }
 </style>
